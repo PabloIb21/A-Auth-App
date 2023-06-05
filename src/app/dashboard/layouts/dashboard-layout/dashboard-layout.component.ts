b4b969204d129db09b963abc4dadbfde77f4e64c
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2023-06-04 19:47:46 -0600

    app ready

---
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject, computed } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+
+@Component({
+  templateUrl: './dashboard-layout.component.html',
+  styleUrls: ['./dashboard-layout.component.css']
+})
+export class DashboardLayoutComponent {
+  private authService = inject(AuthService);
+  public user = computed(() => this.authService.currentUser());
+
+  onLogout() {
+    this.authService.logout();
+  }
+}
